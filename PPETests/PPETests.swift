a4c8a97088464b07bc54d9ce381e7c42c6fa46e0
SSSiye <tldp6036@naver.com>
2025-11-14 14:27:54 +0900

    Initial Commit

---
+++ PPETests/PPETests.swift
@@ -0,0 +1,17 @@
+//
+//  PPETests.swift
+//  PPETests
+//
+//  Created by siye on 11/14/25.
+//
+
+import Testing
+@testable import PPE
+
+struct PPETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
